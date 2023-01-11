c049ec36fe9ade5fef4bdb9ca13e76d9db7c9ded
Rituparno <rituparnodas21@gmail.com>
2023-01-12 00:38:34 +0530

    Open Closed Principle

--- SolidDesignPrinciples/SolidDesignPrinciples.cpp
+++ SolidDesignPrinciples/SolidDesignPrinciples.cpp
@@ -4,21 +4,37 @@
 #include <vector>
 #include <boost/lexical_cast.hpp>
 #include "SingleResponsiblityPrinciple.h"
+#include "OpenClosedPrinciple.h"
 
 int main()
 {
-    Journal journal{ "Dear Diary" };
-    journal.add_entry("I Made Friends");
-    journal.add_entry("I cried alone");
-    journal.add_entry("I pick myself alone");
+    Product apple{ "Apple", Color::green, Size::small };
+    Product tree{ "Tree", Color::green, Size::large };
+    Product house{ "House", Color::blue, Size::large };
 
-    //journal.save("diary.txt");
+    const vector<Product*> all{ &apple, &tree, &house };
 
-    PersistenceManager pm;
-    pm.save(journal, "diary.txt");
+    BetterFilter bf;
+    ColorSpecification green(Color::green);
+    auto green_things = bf.filter(all, green);
+    for (auto& x : green_things)
+        cout << x->name << " is green\n";
 
 
-    std::cout << "Hello World" << std::endl;
+    SizeSpecification large(Size::large);
+    AndSpecification<Product> green_and_large(green, large);
 
+    //auto big_green_things = bf.filter(all, green_and_large);
+
+    // use the operator instead (same for || etc.)
+    auto spec = green && large;
+    for (auto& x : bf.filter(all, spec))
+        cout << x->name << " is green and large\n";
+
+    // warning: the following will compile but will NOT work
+    // auto spec2 = SizeSpecification{Size::large} &&
+    //              ColorSpecification{Color::blue};
+
+    //getchar();
     return 0;
 }
